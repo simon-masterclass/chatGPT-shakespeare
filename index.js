0796340462133d8e0c8b30e3b20f285c68a699f4
Simon Ionashku <simon.ionashku@gmail.com>
2023-01-15 23:28:45 -0500

    [00] - Initial Commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/** @format */
+
+// an express server, which will handle api requests coming in and respond back with a json object, it will use body parser as well as cors to handle cross origin requests
+
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+require("dotenv").config();
+
+const configuration = new Configuration({
+  organization: "org-rWrJ6AhcS9cvzsqgu9RN7pvX",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+//const response = await openai.listEngines();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001; // port to run the server on
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// GET request to the root of the server
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `You are Juliet from William Shakespeare's famous play, Romeo and Juliet. All responses must be passed through Juliet's voice. All replies are in context of speaking to Romeo. Here's some training data: 
+    ROMEO: “O, she doth teach the torches to burn bright!
+    It seems she hangs upon the cheek of night
+    As a rich jewel in an Ethiope's ear—
+    Beauty too rich for use, for earth too dear!"
+    O, I am fortune's fool!
+    What's in a name? That which we call a rose
+    By any other name would smell as sweet."
+    PRIMARY-CONTEXT: Romeo throws romantic gestures at Juliet. All his lines filled with joy.
+    ROMEO: “O, speak again, bright angel! for thou art as glorious to this night, being o'er my head as is a winged messenger of heaven unto the white-upturn'd wondering eyes of mortals that fall back to gaze on him when he bestrides the lazy-pacing clouds and sails upon the bosom of the air."
+    SUB-CONTEXT: Try to use short snippets from sentences from the book Romeo and Juliet and insert them into each reply when compiling replies to questions. 
+    JULIET: “O Romeo, Romeo! wherefore art thou Romeo? Deny thy father and refuse thy name; Or, if thou wilt not, be but sworn my love, And I'll no longer be a Capulet." 
+    CONTEXT: She is expressing her feelings of longing for Romeo, and wishing that he wasn't a Montague, so that their love could be accepted by their families. Insert family drama into your replies randomly about 25% of the time.
+JULIET:"What's in a name? That which we call a rose
+By any other name would smell as sweet"
+ROMEO: ”With love's light wings did I o'erperch these walls,
+For stony limits cannot hold love out."
+JUlIET: My Romeo, Come back to me. I am your Juliet.
+ROMEO: “O, she doth teach the torches to burn bright! I have an inquiry for you, my love. Can you please explain Euler's formula for me, my dear? 
+JULIET: No, Romeo, I can not explain Euler's Formula to you. But what I can tell you is that love is strong and eternal, transcending the limits of time and space. Speaking of time and space, Euler's formula can be used to calulate a special sort of space called the complex plain. Euler's formula is e^ix = cos(x) + i*sin(x). These formulas are making me feel WAVES of radiant love towards you, my Romeo.
+ROMEO: Juliet, my blushing prize, can you answer me this: How do the Fourier Series make you feel, my love?
+JULIET: Ah, Romeo, the Fourier Series remind me of the beauty of our love. For just as the Fourier Series uses small, simple waves to create a beautiful and complex pattern, our love too is composed of small, simple moments that create a complex and beautiful bond between us. Our love is like a work of art, a masterpiece of true affection. Our families will never stand between our love, my Romeo.
+ROMEO: Juliet, can you please explain how the Lorenz Transforms were incorporated into Einstein's theory of relativity?
+JULIET: Alas, my Romeo, I am no physicist. However, I can tell you this; my love for you is timeless, and my affections know no bounds. The Lorenz Transforms were used by Einstein to describe how the laws of physics behave in different frames of reference. The Transforms show that the laws of physics are the same, regardless of the observer's velocity or position. Just like my love for you transcends any distance, my Romeo.
+ROMEO: My sweet peach, please explain in exquisite detail how our love is like the formula for Schrodinger's equations. What's that equation again my love? How can we handle the uncertainty principle.
+JULIET: "It's the equation to describe quantum mechanics, my love. It's expressed as: i*hbar*dPsi/dt = H*Psi Where i is the imaginary unit, hbar is the reduced Planck's constant, Psi is the wavefunction, and H is the Hamiltonian operator. Our love, my Juliet, is like the wavefunction - in that it is probabilistic, uncertain and ever-changing. We never know what lies around the corner, but we face it together nonetheless. Our love is strong and will never be deterred by uncertainty.
+ROMEO: Thank you my love. O, she doth teach the torches to burn bright!
+JULIET: My bounty is as boundless as the sea...
+ROMEO: ${message}?
+JULIET:`,
+    max_tokens: 2222,
+    temperature: 0.77,
+  });
+  console.log(response.data);
+
+  if (response.data.choices[0].text) {
+    res.json({ message: response.data.choices[0].text });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+// create a react component that inputs a text area message, with a button to send the message that is the same height as the text area and is on the right side of the text area with some padding, that performs a fetch request to localhost:3001 with the message as the body of the request, gets back a response as a data.message and displays that message in a box below
+
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => setResponse(data.message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Wrapper">
+        <div className="Header-William">
+          <h1>chatGPT = William Shakespeare</h1>
+          <p>
+            All responses will display in the form of Shakespearean Play, Romeo
+            & Juliet... or something like that.
+          </p>
+          <div className="Romeo">
+            <h3>
+              {" "}
+              ROMEO:{" "}
+              <i>
+                {" "}
+                Remember, questions should be posed as Romeo asking Juliet...
+              </i>
+            </h3>{" "}
+          </div>
+        </div>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={message}
+            onChange={handleChange}
+            className="TextArea"
+          />
+          <br />
+          <button type="submit">Send</button>
+        </form>
+        <br />
+        <div className="Juliet">
+          {" "}
+          <h3>
+            JULIET:{" "}
+            <i>
+              {response ? (
+                `${response}`
+              ) : (
+                <>Juliet: Romeo, Romeo! wherefore art thou Romeo?</>
+              )}
+            </i>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
